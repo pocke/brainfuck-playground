1ecfdb02fe7eaca9ffc4dce9375ae89018357bc6
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-08-15 16:59:59 +0900

    Add deploy script

---
+++ deploy.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+
+def exec(cmd)
+  puts cmd
+  system cmd
+end
+
+unless `git status --short`.chomp.empty?
+  puts 'working directory does not clean!'
+  exit 1
+end
+
+
+puts 'start deploy'
+puts
+
+
+files = %w[
+  dst/main.js
+  node_modules/bootstrap/dist/css/bootstrap.min.css
+  node_modules/lodash/index.js
+  node_modules/vue/dist/vue.js
+]
+
+puts "start git add"
+puts
+
+files.each do |f|
+  cmd =  "git add --force #{f}"
+  unless exec(cmd)
+    puts "`#{cmd}` failed."
+    exit 1
+  end
+end
+
+unless exec 'git commit -m "deploy by script"'
+  puts 'git commit failed.'
+  exit 1
+end
+
+
+exec "git push --force origin #{`git symbolic-ref --short HEAD`.chomp}:gh-pages"
+
+exec 'git reset @~'
