6e466e5080a5981117737986444a0ab1dd197950
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-08-26 10:47:37 +0900

    Define query-string signature

---
+++ query-string.d.ts
@@ -0,0 +1,3 @@
+export declare function parse(search: string): {[key: string]: string};
+export declare function stringify(obj: {[key: string]: string}): string;
+export declare function extract(url: string): string;
