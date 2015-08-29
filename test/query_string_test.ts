f93d1c99e36c9e10fb13c1cc82b2d0fee6b24d0a
Masataka Kuwabara <p.ck.t22@gmail.com>
2015-08-29 16:29:41 +0900

    Fix test

--- test/query_string_test.ts
+++ test/query_string_test.ts
@@ -2,18 +2,19 @@ import * as mocha from "mocha";
 import * as assert from "power-assert";
 
 import * as la from '../src/language';
+import * as qs from '../src/query_string';
 
 
-describe('ParseQueryString and StringifyQueryString', () => {
+describe('Parse and Stringify', () => {
   it('should parse', () => {
-    const q: UI.QueryString = {
+    const q: qs.QueryString = {
       program: 'hoge',
       lang:    la.DEFAULT_LANGUAGE,
       input:   [65, 66, 67, 68],
     };
-    const se = UI.StringifyQueryString(q);
+    const se = qs.Stringify(q);
     history.pushState(null, null, `${location.protocol}//${location.host}${location.pathname}?${se}`);
-    const parsed = UI.ParseQueryString();
+    const parsed = qs.Parse();
     assert.deepEqual(q, parsed);
   });
 });
